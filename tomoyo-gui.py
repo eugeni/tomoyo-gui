ac9ef349a622126d8862b1b4f723c47df861d5da
Eugeni Dodonov <eugeni@mandriva.com>
2009-07-06 09:54:13 -0300

    Added support for basic domain visualization.

--- tomoyo-gui.py
+++ tomoyo-gui.py
@@ -26,11 +26,15 @@ class TomoyoGui(gtk.Window):
 
         self.policy = policy
 
+        # main vbox
+        self.main_vbox = gtk.VBox()
+        self.add(self.main_vbox)
+
         # scrolled window
         sw = gtk.ScrolledWindow()
         sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
         sw.set_shadow_type(gtk.SHADOW_ETCHED_IN)
-        self.add(sw)
+        self.main_vbox.pack_start(sw)
 
         # list of options
         lstore = gtk.ListStore(
@@ -43,7 +47,7 @@ class TomoyoGui(gtk.Window):
         treeview.set_rules_hint(True)
         treeview.set_search_column(self.COLUMN_DOMAIN)
 
-        treeview.connect('row-activated', self.option_changed, lstore)
+        treeview.connect('row-activated', self.show_domain, lstore)
 
         # configuring columns
 
@@ -72,21 +76,74 @@ class TomoyoGui(gtk.Window):
                     self.COLUMN_CUSTOM, pango.WEIGHT_NORMAL
                     )
 
+        # contents
+        sw2 = gtk.ScrolledWindow()
+        sw2.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        sw2.set_shadow_type(gtk.SHADOW_ETCHED_IN)
+        frame = gtk.Frame(_("Domain details"))
+        self.domain_details = gtk.VBox(False, 5)
+        frame.add(self.domain_details)
+        sw2.add_with_viewport(frame)
+        self.main_vbox.pack_start(sw2)
 
-        sw.add(treeview)
+        # size group
+        self.size_group = gtk.SizeGroup(gtk.SIZE_GROUP_HORIZONTAL)
+
+
         self.show_all()
 
-    def option_changed(self, treeview, path, col, model):
-        """Processes an option change"""
+    def __add_row(self, table, row, label_text, options):
+        label = gtk.Label(label_text)
+        label.set_use_underline(True)
+        label.set_alignment(0, 1)
+        table.attach(label, 0, 1, row, row + 1, gtk.EXPAND | gtk.FILL, 0, 0, 0)
+
+        self.size_group.add_widget(options)
+        table.attach(options, 1, 2, row, row + 1, 0, 0, 0, 0)
+
+
+    def show_domain(self, treeview, path, col, model):
+        """Shows details for a domain"""
         iter = model.get_iter(path)
         domain = model.get_value(iter, self.COLUMN_DOMAIN)
-        value = model.get_value(iter, self.COLUMN_STATE)
+        params = self.policy.policy_dict[domain]
+
+        children = self.domain_details.get_children()
+        for child in children:
+            self.domain_details.remove(child)
+
+        label = gtk.Label(domain)
+        label.set_line_wrap(True)
+        self.domain_details.pack_start(label, False, False)
+        # building details
+        table = gtk.Table(2, 2, False)
+
+        # profile
+        combobox = gtk.combo_box_new_text()
+        combobox.append_text(_("Disabled"))
+        combobox.append_text(_("Learning"))
+        combobox.append_text(_("Permissive"))
+        combobox.append_text(_("Enforcing"))
+        combobox.set_active(self.policy.get_profile(domain))
+        self.__add_row(table, 1, _("Profile"), combobox)
+
+        self.domain_details.add(table)
+        self.domain_details.show_all()
+
 
 class TomoyoPolicy:
     def __init__(self):
         """Initializes the policy class"""
         # TODO: support system/saved policy
         pass
+
+    def get_profile(self, item):
+        """Gets profile status for item"""
+        params = self.policy_dict.get(item, None)
+        for p,val in params:
+            if p == 'use_profile':
+                return int(val)
+        return 0
 
     def parse(self):
         """Loads the policy"""
